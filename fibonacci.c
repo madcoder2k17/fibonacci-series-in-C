2604ca931ef11716af845fe15cd4a1b89b76dbb8
Joy <madcoder2k17@users.noreply.github.com>
2017-07-03 21:18:15 +0600

    @grapie
    Thanks™

---
+++ fibonacci.c
@@ -0,0 +1,63 @@
+/*Using loops*/
+
+#include<stdio.h>
+ 
+int main()
+{
+   int n, first = 0, second = 1, next, c;
+ 
+   printf("Enter the number of terms\n");
+   scanf("%d",&n);
+ 
+   printf("First %d terms of Fibonacci series are :-\n",n);
+ 
+   for ( c = 0 ; c < n ; c++ )
+   {
+      if ( c <= 1 )
+         next = c;
+      else
+      {
+         next = first + second;
+         first = second;
+         second = next;
+      }
+      printf("%d\n",next);
+   }
+ 
+   return 0;
+}
+
+
+
+2. using recursion
+
+#include<stdio.h>
+ 
+int Fibonacci(int);
+ 
+main()
+{
+   int n, i = 0, c;
+ 
+   scanf("%d",&n);
+ 
+   printf("Fibonacci series\n");
+ 
+   for ( c = 1 ; c <= n ; c++ )
+   {
+      printf("%d\n", Fibonacci(i));
+      i++; 
+   }
+ 
+   return 0;
+}
+ 
+int Fibonacci(int n)
+{
+   if ( n == 0 )
+      return 0;
+   else if ( n == 1 )
+      return 1;
+   else
+      return ( Fibonacci(n-1) + Fibonacci(n-2) );
+} 
